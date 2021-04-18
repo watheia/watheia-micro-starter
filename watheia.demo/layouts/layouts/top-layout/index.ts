bfccf595e0be4086ddbb68550f7f79fdcb98eee8
Aaron R Miller <amiller@watheia.org>
2021-04-18 22:51:07 +0000

    remove duplicate

--- watheia.demo/layouts/layouts/top-layout/index.ts
+++
@@ -1,2 +0,0 @@
-export { TopLayout } from './top-layout';
-export type { TopLayoutProps } from './top-layout';
--- watheia.demo/layouts/layouts/top-layout/top-layout.composition.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { TopLayout } from './top-layout';
-
-export const BasicTopLayout = () => (
-  <TopLayout text="hello from TopLayout" />
-);
