98d7bc7ec0ce4d17816ca75f164680c434985c05
Aaron R Miller <amiller@watheia.org>
2021-04-19 03:24:36 +0000

    cleanup docker-compose

--- demo/next.config.js
+++ demo/next.config.js
@@ -1,5 +1,23 @@
+const { BLOG_URL } = process.env
+
 module.exports = {
   future: {
     webpack5: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/blog',
+        destination: `${BLOG_URL}/blog`,
+      },
+      {
+        source: '/blog/:path*',
+        destination: `${BLOG_URL}/blog/:path*`,
+      },
+    ]
   },
 }
